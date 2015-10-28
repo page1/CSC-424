152b82a224be3d1805342704b338ba796d736aeb
Scott Page <scott.page@raise.com>
2015-10-27 22:33:45 -0500

    analyse omnipod
    provide join for fitbit data

--- do.R
+++ do.R
@@ -1,1 +1,27 @@
-# Us this to run things from the top level
+# Use this to run things from the top level
+source("get.R")
+source("munge.R")
+source("analyze.R")
+
+library(ggplot2)
+library(dplyr)
+
+# Get Data
+omni_pod <- get_omnipod_data()
+floors <- get_floors()
+distance <- get_distance()
+calories <- get_calories()
+steps <- get_steps()
+elevation <- get_elevation()
+
+# Mutate Data
+omni_pod <- column_names_to_lower_case(omni_pod)
+
+fitbit <- join_fitbit_data(floors, distance, calories, steps, elevation)
+
+# Analyse Data
+
+plot_omnipod_type_frequency(omni_pod)
+
+
+
--- munge.R
+++ munge.R
@@ -1,1 +1,17 @@
 #Use this to do data cleaning and manipulation
+
+column_names_to_lower_case <- function(data) {
+  colnames(data) <- tolower(colnames(data))
+  
+  return(data)
+}
+
+join_fitbit_data <- function(floors, distance, calories, steps, elevation){
+  data <- floors %>%
+    full_join(distance, by = c("date" = "date", "time" = "time")) %>%
+    full_join(calories, by = c("date" = "date", "time" = "time")) %>%
+    full_join(steps, by = c("date" = "date", "time" = "time")) %>%
+    full_join(elevation, by = c("date" = "date", "time" = "time"))
+  
+  return(data)
+}
