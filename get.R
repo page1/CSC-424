f2f865804fbbc955b8d71fc4f14cbe58ba54ad2c
chenran2119 <chenran2119@gmail.com>
2015-10-27 21:21:23 -0500

    add a line before return

--- get.R
+++ get.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 get_omnipod_data <- function(){
   data <- read.csv("Data/OmniPod.csv")
+  
   return(data)
 }
 
@@ -15,6 +16,7 @@ read_fitbit_folder <- function(folder_name){
   file_names <- list.files(folder_name)
   data <- lapply(paste(folder_name, file_names, sep = "/"), read.csv) %>%
     do.call("rbind", .)
+  
  return(data)
 }
 
@@ -22,5 +24,6 @@ read_floor_folder<-function(){
   data<-read_fitbit_folder ("Data/floors")
   data<-select(data,date,time,value) %>%
     rename(floors=value)
+  
   return(data)
 }
