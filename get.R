a6c55c365ec1ee7eaf627b8b1437d2256101adda
danoPA <daasland@gmail.com>
2015-10-28 16:08:29 -0500

    testing to see if this works for commiting changes via Rstudio

--- get.R
+++ get.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 get_omnipod_data <- function(){
   data <- read.csv("Data/OmniPod.csv")
+  
   return(data)
 }
 
@@ -16,13 +17,45 @@ read_fitbit_folder <- function(folder_name){
   data <- lapply(paste(folder_name, file_names, sep = "/"), read.csv) %>%
     do.call("rbind", .)
   
+ return(data)
+}
+
+get_floors <- function(){
+  data<-read_fitbit_folder ("Data/floors")
+  data<-select(data,date,time,value) %>%
+    rename(floors=value)
+  
   return(data)
+}
+
+get_distance <- function(){
+  data <- read_fitbit_folder("Data/distance")
+  data <- select(data, date, time, value) %>%
+    rename(distance = value)
+  
+    return(data)
 }
 
 get_calories <- function(){
   data <- read_fitbit_folder("Data/calories")
   data <- select(data, date, time, value) %>%
     rename(calories = value)
+  
+  return(data)
+}
+
+get_steps <- function(){
+  data <- read_fitbit_folder("Data/steps")
+  data <- select(data, date, time, value) %>%
+    rename(steps = value)
+  
+  return(data)
+}
+
+get_elevation <- function(){
+  data <- read_fitbit_folder("Data/elevation")
+  data <- select(data, date, time, value) %>%
+    rename(elevation = value)
   
   return(data)
 }
