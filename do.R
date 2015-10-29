a93edb5843827687bf8c3ae14e7fbeef1b249af5
Scott Page <scott.page@raise.com>
2015-10-29 09:36:25 -0500

    munge omnipod

--- do.R
+++ do.R
@@ -6,17 +6,18 @@ source("analyze.R")
 library(dplyr)
 
 # Get Data
-omni_pod <- get_omnipod_data()
-floors <- get_floors()
-distance <- get_distance()
-calories <- get_calories()
-steps <- get_steps()
-elevation <- get_elevation()
+omni_pod_raw <- get_omnipod_data()
+dexcom_raw <- get_dexcom()
+floors_raw <- get_floors()
+distance_raw <- get_distance()
+calories_raw <- get_calories()
+steps_raw <- get_steps()
+elevation_raw <- get_elevation()
 
 # Mutate Data
-omni_pod <- munge_omni_pod(omni_pod)
+omni_pod <- munge_omni_pod(omni_pod_raw)
 
-fitbit <- join_fitbit_data(floors, distance, calories, steps, elevation)
+fitbit <- join_fitbit_data(floors_raw, distance_raw, calories_raw, steps_raw, elevation_raw)
 
 # Analyse Data
 
