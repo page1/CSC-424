39849d0052f48496c311e837950bd21ec5f37f66
Scott Page <scott.page@raise.com>
2015-10-28 19:40:11 -0500

    Scott initial analysis

--- munge.R
+++ munge.R
@@ -1,9 +1,29 @@
 #Use this to do data cleaning and manipulation
+library(dplyr)
+library(tidyr)
 
 column_names_to_lower_case <- function(data) {
   colnames(data) <- tolower(colnames(data))
   
   return(data)
+}
+
+split_units <- function(data){
+  data <- data %>%
+    mutate(value = ifelse(value == "", " ", value)) %>%
+    separate(value, c("value", "units"), sep = " ") %>%
+    mutate(units = gsub("\\(|\\)", "", units))
+  
+  return(data)
+}
+
+munge_omni_pod <- function(omni_pod){
+  omni_pod <- omni_pod %>%
+    column_names_to_lower_case() %>%
+    select(-hidden) %>%
+    split_units() %>%
+    mutate(date = as.Date(date, format = "%m/%d/%Y"),
+           value = as.numeric(value))
 }
 
 join_fitbit_data <- function(floors, distance, calories, steps, elevation){
