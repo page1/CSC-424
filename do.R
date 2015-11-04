87976134e5144e589436363d529c834f6692ee1b
Scott Page <scott.page@raise.com>
2015-11-03 21:48:57 -0600

    join fitbit and dexcom

--- do.R
+++ do.R
@@ -15,9 +15,15 @@ steps_raw <- get_steps()
 elevation_raw <- get_elevation()
 
 # Mutate Data
+dexcom <- munge_dexcom(dexcom_raw)
 omni_pod <- munge_omni_pod(omni_pod_raw)
 
 fitbit <- join_fitbit_data(floors_raw, distance_raw, calories_raw, steps_raw, elevation_raw)
+fitbit <- munge_fitbit(fitbit)
+
+fitbit_dexcom <- join_fitbit_with_dexcom_predictions(fitbit, dexcom)
+
+
 
 # Analyse Data
 
